8a693d9a0639b851cb048f877ed3cb7f9214c9cb
Vassista <vassista@icloud.com>
2024-07-13 21:24:04 +0530

    update code with string concatenation

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,4 +1,5 @@
 # Description: This program generates a band name based on the user's input.
 city_name = str(input("Enter the city you've grown in: "))
 pet_name = str(input("Enter the name of your pet: "))
-print(f"Your band name could be: {city_name} {pet_name}")
+band_name = city_name + " " + pet_name
+print(f"Your band name could be: {band_name}")
