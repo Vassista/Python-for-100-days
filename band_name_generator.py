40c3a9b2c413ec96df8fccf0fac1cf38d84bc163
Vassista <vassista@icloud.com>
2024-07-13 20:52:08 +0530

    Day 1 of python challenge

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+# Description: This program generates a band name based on the user's input.
+city_name = str(input("Enter the city you've grown in: "))
+pet_name = str(input("Enter the name of your pet: "))
+print(f"Your band name could be: {city_name} {pet_name}")
