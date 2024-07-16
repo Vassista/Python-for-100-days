414c42cf8f0d00c79616bd5e6ceea7f4bc704479
Vassista <vassista@icloud.com>
2024-07-16 21:37:36 +0530

    format output to "2" decimal places

--- BMI 2.0.py
+++ BMI 2.0.py
@@ -8,13 +8,13 @@ weight = int(input())
 bmi = float(weight / height ** 2)
 #Under 18.5 they are underweight
 if bmi < 18.5:
-    print(f"Your BMI is {bmi}, you are underweight.")
+    print(f"Your BMI is {bmi:.2f}, you are underweight.")
 #Between 18.5 and 25 they have a normal weight
 elif bmi < 25:
-    print(f"Your BMI is {bmi}, you have a normal weight.")
+    print(f"Your BMI is {bmi:.2f}, you have a normal weight.")
 #Between 25 and 30 they are slightly overweight
 elif bmi < 30:
-    print(f"Your BMI is {bmi}, you are slightly overweight.")
+    print(f"Your BMI is {bmi:.2f}, you are slightly overweight.")
 #Between 30 and 35 they are obese
 elif bmi < 35:
     print(f"Your BMI is {bmi}, you are obese.")
