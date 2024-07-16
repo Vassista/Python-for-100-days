220bd3c6bda376c33dfc8753ba034e4ef105ff74
Vassista <vassista@icloud.com>
2024-07-16 18:36:43 +0530

    fixed a bug in print function [ line 7 ]

--- tip-calculator.py
+++ tip-calculator.py
@@ -4,4 +4,4 @@ tip = tip_percentage/100 * bill
 bill += tip
 total_people= int(input("How many people to split the bill? "))
 each_person_bill = round(bill / total_people,2)
-print(f"Each pʼʼerson should pay: ${each_person_bill:.2f}") ## The `:.2f` part o..f the expression is a format specifier that specifies the precision of the floating-point number
+print(f"Each person should pay: ${each_person_bill:.2f}") ## The `:.2f` part o..f the expression is a format specifier that specifies the precision of the floating-point number
