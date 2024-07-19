a1a814e83ad863dff65074bc15d54d57d4df2f75
Vassista <vassista@icloud.com>
2024-07-19 23:59:33 +0530

    Day 5 of python challenge

---
+++ Day 6/Reeborg Maze.py
@@ -0,0 +1,22 @@
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+         turn_right()
+         move()
+    elif front_is_clear():
+        move()
+    else:
+         turn_left()
+
+#This code does't work here. This is the solution for the reeborg's final test at https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds/menus/reeborg_intro_en.json&name=Maze&url=worlds/tutorial_en/maze1.json
+
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
